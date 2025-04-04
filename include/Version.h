b6300cf6513bec5b2cf3e8f323981faf64178581
xit303 <d.lange303@gmail.com>
2025-04-04 13:41:53 +0200

    feat: Add versioning support with Version.h and integrate into main.cpp

---
+++ include/Version.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define VERSION_MAJOR 1
+#define VERSION_MINOR 0
+#define VERSION_PATCH 0
+#define VERSION_STRING "v" + std::to_string(VERSION_MAJOR) + "." + std::to_string(VERSION_MINOR) + "." + std::to_string(VERSION_PATCH)
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,7 @@
 #include <iostream>
+#include "Version.h"
 #include "CmdLineParser.h"
 #include "XMakefileParser.h"
-
-#define VERSION_MAJOR 1
-#define VERSION_MINOR 0
-#define VERSION_PATCH 0
-#define VERSION_STRING "v" + std::to_string(VERSION_MAJOR) + "." + std::to_string(VERSION_MINOR) + "." + std::to_string(VERSION_PATCH)
 
 int main(int argc, char **argv)
 {
