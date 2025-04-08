b9a9f967d1e4fc660c8e3bddee49e2953030c398
Lange, Dennis <dennis.lange@mayr.de>
2025-04-08 08:49:33 +0200

    refactor: Consolidate JSON parsing logic into FromJSON methods for XMakefile and XMakefileConfig

--- include/XMakefileParser.h
+++ include/XMakefileParser.h
@@ -20,6 +20,46 @@ public:
     std::vector<std::string> LibraryPaths;
     std::vector<std::string> Libraries;
     std::vector<std::string> SourcePaths;
+
+    void FromJSON(const JsonVariant &doc)
+    {
+        Name = doc["name"].as<std::string>();
+        BuildType = doc["build_type"].as<std::string>();
+        BuildDir = doc["build_dir"].as<std::string>();
+        OutputFilename = doc["output_filename"].as<std::string>();
+        CompilerPath = doc["compiler_path"].as<std::string>();
+        Compiler = doc["compiler"].as<std::string>();
+        CompilerFlags = doc["compiler_flags"].as<std::string>();
+        LinkerFlags = doc["linker_flags"].as<std::string>();
+
+        // Extract include paths
+        JsonArray includePaths = doc["include_paths"].as<JsonArray>();
+        for (JsonVariant path : includePaths)
+        {
+            IncludePaths.push_back(path.as<std::string>());
+        }
+
+        // Extract library paths
+        JsonArray libraryPaths = doc["library_paths"].as<JsonArray>();
+        for (JsonVariant path : libraryPaths)
+        {
+            LibraryPaths.push_back(path.as<std::string>());
+        }
+
+        // Extract libraries
+        JsonArray libraries = doc["libraries"].as<JsonArray>();
+        for (JsonVariant lib : libraries)
+        {
+            Libraries.push_back(lib.as<std::string>());
+        }
+
+        // Extract source paths
+        JsonArray sourcePaths = doc["source_paths"].as<JsonArray>();
+        for (JsonVariant path : sourcePaths)
+        {
+            SourcePaths.push_back(path.as<std::string>());
+        }
+    }
 };
 
 class XMakefile
@@ -32,6 +72,24 @@ public:
     std::string xmakeFileLicense;
 
     std::vector<XMakefileConfig> configs; // List of configurations
+
+    void FromJSON(const JsonVariant &doc)
+    {
+        xmakeFileName = doc["name"].as<std::string>();
+        xmakeFileVersion = doc["version"].as<std::string>();
+        xmakeFileDescription = doc["description"].as<std::string>();
+        xmakeFileAuthor = doc["author"].as<std::string>();
+        xmakeFileLicense = doc["license"].as<std::string>();
+
+        // Extract configurations
+        JsonArray configsArray = doc["configurations"].as<JsonArray>();
+        for (JsonVariant config : configsArray)
+        {
+            XMakefileConfig cfg;
+            cfg.FromJSON(config);
+            configs.push_back(cfg);
+        }
+    }
 };
 
 class XMakefileParser
--- src/XMakefileParser.cpp
+++ src/XMakefileParser.cpp
@@ -36,56 +36,7 @@ bool XMakefileParser::Parse(const std::string &path)
     }
 
     // Extract the xmakefile metadata
-    xmakefile.xmakeFileName = jsonDoc["name"].as<std::string>();
-    xmakefile.xmakeFileVersion = jsonDoc["version"].as<std::string>();
-    xmakefile.xmakeFileDescription = jsonDoc["description"].as<std::string>();
-    xmakefile.xmakeFileAuthor = jsonDoc["author"].as<std::string>();
-    xmakefile.xmakeFileLicense = jsonDoc["license"].as<std::string>();
-
-    // Extract the configurations
-    JsonArray configs = jsonDoc["configurations"].as<JsonArray>();
-
-    for (JsonVariant config : configs)
-    {
-        currentConfig.Name = config["name"].as<std::string>();
-        currentConfig.BuildType = config["build_type"].as<std::string>();
-        currentConfig.BuildDir = config["build_dir"].as<std::string>();
-        currentConfig.OutputFilename = config["output_filename"].as<std::string>();
-        currentConfig.CompilerPath = config["compiler_path"].as<std::string>();
-        currentConfig.Compiler = config["compiler"].as<std::string>();
-        currentConfig.CompilerFlags = config["compiler_flags"].as<std::string>();
-        currentConfig.LinkerFlags = config["linker_flags"].as<std::string>();
-
-        // Extract include paths
-        JsonArray includePaths = config["include_paths"].as<JsonArray>();
-        for (JsonVariant path : includePaths)
-        {
-            currentConfig.IncludePaths.push_back(path.as<std::string>());
-        }
-
-        // Extract library paths
-        JsonArray libraryPaths = config["library_paths"].as<JsonArray>();
-        for (JsonVariant path : libraryPaths)
-        {
-            currentConfig.LibraryPaths.push_back(path.as<std::string>());
-        }
-
-        // Extract libraries
-        JsonArray libraries = config["libraries"].as<JsonArray>();
-        for (JsonVariant lib : libraries)
-        {
-            currentConfig.Libraries.push_back(lib.as<std::string>());
-        }
-
-        // Extract source paths
-        JsonArray sourcePaths = config["source_paths"].as<JsonArray>();
-        for (JsonVariant path : sourcePaths)
-        {
-            currentConfig.SourcePaths.push_back(path.as<std::string>());
-        }
-
-        xmakefile.configs.push_back(currentConfig); // Add the configuration to the list
-    }
+    xmakefile.FromJSON(jsonDoc);
 
     file.close();
 
